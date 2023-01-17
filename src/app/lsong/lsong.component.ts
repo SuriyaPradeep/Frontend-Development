3a844510104e4684265e9e9728891987cad2615a
Suriya <122858374+SuriyaPradeep@users.noreply.github.com>
2023-01-17 00:53:23 -0800

    Create lsong.component.ts

---
+++ src/app/lsong/lsong.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lsong',
+  templateUrl: './lsong.component.html',
+  styleUrls: ['./lsong.component.css']
+})
+export class LsongComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
