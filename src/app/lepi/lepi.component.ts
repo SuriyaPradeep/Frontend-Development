6fbd8a53a163474f82ed762bba052f1bcd434f83
Suriya <122858374+SuriyaPradeep@users.noreply.github.com>
2023-01-17 00:53:44 -0800

    Create lepi.component.ts

---
+++ src/app/lepi/lepi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lepi',
+  templateUrl: './lepi.component.html',
+  styleUrls: ['./lepi.component.css']
+})
+export class LepiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
